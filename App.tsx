001f624c07ced1fa327655b7bee9e54e60e30f49
nelsontgpsi <nelsonalunogpsi@email.com>
2025-12-10 22:40:27 +0000

    Remove FAQ component section

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import { Hero } from './components/Hero';
 import { Services } from './components/Services';
 import { Portfolio } from './components/Portfolio';
 import { WhyUs } from './components/WhyUs';
-import { FAQ } from './components/FAQ';
 import { Footer } from './components/Footer';
 
 const App: React.FC = () => {
@@ -16,7 +15,6 @@ const App: React.FC = () => {
         <Services />
         <Portfolio />
         <WhyUs />
-        <FAQ />
       </main>
       <Footer />
     </div>
